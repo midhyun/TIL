e08938740b4c6f38ee17ddec637e8f092e0c6d47
midhyun <kfree912@gmail.com>
2022-08-30 17:42:04 +0900

    TIL_0830

---
+++ Python/문제풀기/after_bfs/2294_동전2.py
@@ -0,0 +1,15 @@
+import sys
+sys.stdin = open('2294_동전2.txt')
+input = sys.stdin.readline
+
+n, k = map(int,input().split())
+coins = [int(input()) for _ in range(n)]
+dp = [10001]*(k+1)
+dp[0] = 0
+for v in coins:
+    for j in range(v, k+1):
+        dp[j] = min(dp[j-v]+1, dp[j])
+if dp[k] >= 10001:
+    print(-1)
+else:
+    print(dp[k])
