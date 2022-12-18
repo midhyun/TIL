141a861aa4cd5ce68fca85fd6fb05dd3bd5f23bd
midhyun <kfree912@gmail.com>
2022-12-18 23:51:48 +0900

    boj_relocation

---
+++ Python/문제풀기/after_bfs/5901_relocation.py
@@ -0,0 +1,58 @@
+import sys
+from heapq import heappop, heappush
+from itertools import permutations
+sys.stdin = open('5901_relocation.txt')
+input = sys.stdin.readline
+INF = int(1e9)
+N, M, K = map(int, input().split())
+markets = set()
+for _ in range(K):
+    markets.add(int(input()))
+graph = [[] for _ in range(N+1)]
+
+for _ in range(M):
+    a, b, c = map(int, input().split())
+    graph[a].append((c, b))
+    graph[b].append((c, a))
+
+def dijkstra(start):
+    q = []
+    heappush(q, (0, start))
+    visited = [INF] * (N+1)
+    visited[start] = 0
+    while q:
+        dist, node = heappop(q)
+        if dist > visited[node]:
+            continue
+        for dx, x in graph[node]:
+            cost = dist + dx
+            if visited[x] > cost:
+                visited[x] = cost
+                heappush(q, (cost, x))
+    return visited
+
+per = permutations(markets, K)
+result = INF
+visited_list = [0] *(N+1)
+for i in markets:
+    visited_list[i] = dijkstra(i)
+
+for i in range(1, N+1):
+    if i in markets:
+        continue
+    for permu in per:
+        temp = 0
+        now = i
+        for k, v in enumerate(permu):
+            if k == 0:
+                temp += visited_list[v][i]
+                now = v
+            else:
+                temp += visited_list[v][now]
+                now = v
+            print(temp)
+        temp += visited_list[v][i]
+        print(temp)
+        result = min(result, temp)
+    
+print(result)
