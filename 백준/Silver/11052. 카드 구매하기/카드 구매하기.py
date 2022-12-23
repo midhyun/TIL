4a54acb297c78d0c631f77ffa28212d1f5b9ebb9
HyunJung Kim <108651809+midhyun@users.noreply.github.com>
2022-12-23 20:35:21 +0900

    [Silver I] Title: 카드 구매하기, Time: 208 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
@@ -0,0 +1,11 @@
+import sys
+input= sys.stdin.readline
+
+n = int(input())
+p = [0]+[*map(int, input().split())]
+dp = [0]*(n+1)
+for i in range(1, n+1):
+    for j in range(i+1):
+        dp[i] = max(dp[i], dp[i-j] + p[j])
+
+print(dp[n])
