2032df197724807b40f7623a5fb6f6e0fd9c75ed
HyunJung Kim <108651809+midhyun@users.noreply.github.com>
2022-12-24 16:23:17 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 300 ms, Memory: 126088 KB -BaekjoonHub

---
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.py
@@ -0,0 +1,33 @@
+import sys
+from heapq import heappop, heappush
+input = sys.stdin.readline
+
+INF = int(1e9)
+n = int(input())
+m = int(input())
+graph = [[] for _ in range(n+1)]
+visited = [INF] * (n+1)
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a].append((c, b))
+
+A, B = map(int, input().split())
+path = [[] for _ in range(n+1)]
+q = []
+heappush(q, (0, A))
+visited[A] = 0
+path[A] = [A]
+while q:
+    dist, node = heappop(q)
+    if dist > visited[node]:
+        continue
+    for nd, x in graph[node]:
+        cost = dist + nd
+        if visited[x] > cost:
+            visited[x] = cost
+            path[x] = path[node] + [x]
+            heappush(q, (cost, x))
+
+print(visited[B])
+print(len(path[B]))
+print(*path[B])
