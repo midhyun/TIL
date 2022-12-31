d2f9fb221002231a6eb6473907ecf150f0f33517
midhyun <kfree912@gmail.com>
2022-12-31 17:08:38 +0900

    boj_bisect

---
+++ Python/문제풀기/after_bfs/14002_가장 긴 증가하는 부분 수열4.py
@@ -0,0 +1,18 @@
+import sys
+import bisect
+sys.stdin = open('12738_가장 긴 증가하는 부분 수열3.txt')
+input = sys.stdin.readline
+
+n = int(input())
+nums = [*map(int, input().split())]
+dp = [nums[0]]
+
+for i in range(1, n):
+    if nums[i] > dp[-1]:
+        dp.append(nums[i])
+    else:
+        idx = bisect.bisect_left(dp, nums[i])
+        dp[idx] = nums[i]
+
+print(len(dp))
+print([*dp])
