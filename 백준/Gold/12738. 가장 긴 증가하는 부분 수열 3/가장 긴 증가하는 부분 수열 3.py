f4855fc5b81a4989df2133f1bb97c7ced078a3fe
HyunJung Kim <108651809+midhyun@users.noreply.github.com>
2023-01-01 21:00:55 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 656 ms, Memory: 265192 KB -BaekjoonHub

---
+++ 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.py
@@ -0,0 +1,16 @@
+import sys
+import bisect
+input = sys.stdin.readline
+
+n = int(input())
+nums = [*map(int, input().split())]
+dp = [nums[0]]
+
+for i in range(1, n):
+    if nums[i] > dp[-1]:
+        dp.append(nums[i])
+    else:
+        idx = bisect.bisect_left(dp, nums[i])
+        dp[idx] = nums[i]
+
+print(len(dp))
