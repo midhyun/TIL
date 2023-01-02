95b5906593ba8b45a0f5de1bcd0667c003e37d64
midhyun <kfree912@gmail.com>
2023-01-02 16:46:11 +0900

    LIS_bisect_dp

--- Python/문제풀기/after_bfs/14003_가장 긴 증가하는 부분 수열5.py
+++ Python/문제풀기/after_bfs/14003_가장 긴 증가하는 부분 수열5.py
@@ -1,17 +1,28 @@
 import sys
 import bisect
-sys.stdin = open('14003_가장 긴 증가하는 부분 수열.txt')
+sys.stdin = open('14003_가장 긴 증가하는 부분 수열5.txt')
 input = sys.stdin.readline
 
 n = int(input())
 nums = [*map(int, input().split())]
-dp = [1] * n
-result = [nums[0]]
+result = [1] * n
+dp = [nums[0]]
 
 for i in range(1, n):
-    if nums[i] > result[-1]:
-        result.append(nums[i])
-        dp[i] = dp[i-1] + 1
+    if nums[i] > dp[-1]:
+        dp.append(nums[i])
+        result[i] = len(dp)
     else:
-        idx = bisect.bisect_left(result, nums[i])
+        idx = bisect.bisect_left(dp, nums[i])
+        dp[idx] = nums[i]
+        result[i] = idx + 1
         
+temp = len(dp)
+print(temp)
+res = []
+for i in range(n-1, -1, -1):
+    if result[i] == temp:
+        res.append(nums[i])
+        temp -= 1
+res.reverse()
+print(*res)
