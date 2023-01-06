6c3d6f11e55deb27fc3198911d3a02deff25965b
HyunJung Kim <108651809+midhyun@users.noreply.github.com>
2023-01-06 23:37:46 +0900

    [Gold III] Title: 앱, Time: 152 ms, Memory: 123092 KB -BaekjoonHub

---
+++ 백준/Gold/7579. 앱/앱.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+memories =[0] + [*map(int, input().split())]
+cost =[0] + [*map(int, input().split())]
+length = sum(cost) + 1
+dp = [[0]*length for _ in range(n+1)]
+ans = 1e9
+
+for i in range(1, n+1):
+    cost_i, memory_i = cost[i], memories[i]
+    for j in range(length):
+        dp[i][j] = dp[i-1][j]
+    for j in range(cost_i, length):
+        dp[i][j] = max(dp[i-1][j-cost_i] + memory_i, dp[i][j])
+        if dp[i][j] >= m:
+            ans = min(ans, j)
+
+print(ans)
