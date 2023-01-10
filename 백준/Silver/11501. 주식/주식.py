2c09e228b20cd7ce2388e6ff499bf93cc6c89681
HyunJung Kim <108651809+midhyun@users.noreply.github.com>
2023-01-10 18:25:55 +0900

    [Silver II] Title: 주식, Time: 3268 ms, Memory: 167220 KB -BaekjoonHub

---
+++ 백준/Silver/11501. 주식/주식.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    prices = [*map(int, input().split())]
+    temp = prices[-1]
+    result = 0
+    for i in range(n-2, -1, -1):
+        if temp > prices[i]:
+            result += temp - prices[i]
+        else:
+            temp = prices[i]
+    print(result)
