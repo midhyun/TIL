f01fac78029d0583857545c5113338f168785898
HyunJung Kim <108651809+midhyun@users.noreply.github.com>
2023-01-10 18:43:47 +0900

    [Silver I] Title: 회의실 배정, Time: 324 ms, Memory: 47280 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+lst = []
+for i in range(n):
+    start, end = map(int,input().split())
+    lst.append([start, end])
+lst = sorted(lst, key=lambda x: x[0])
+lst = sorted(lst, key=lambda x: x[1])
+
+last = 0
+cnt = 0
+for i,j in lst:
+    if i >= last:
+        cnt += 1
+        last = j
+print(cnt)
