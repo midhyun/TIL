25339588e79c4fcbc02d2f9bb4742b472383b629
midhyun <kfree912@gmail.com>
2023-01-13 23:46:04 +0900

    그리드_멀티탭스케줄링

--- Python/문제풀기/after_bfs/8980_택배.py
+++ Python/문제풀기/after_bfs/8980_택배.py
@@ -5,19 +5,20 @@ input = sys.stdin.readline
 n, c = map(int, input().split())
 m = int(input())
 
-boxes = [list(map(int, input().split())) for _ in range(m)]
-boxes.sort(key=lambda x:x[1]) # 마을 도착 시간이 빠른 것 순으로 정렬
+order = [list(map(int, input().split())) for _ in range(m)]
+order.sort(key=lambda x:x[1]) # 마을 도착 시간이 빠른 것 순으로 정렬
 
 count = 0
 remains = [c]*(n+1) # 마을 당 수용가능한 박스 수
-
+print(order)
 for i in range(m):
     temp = c # c개를 옮길 수 있다고 가정
-    for j in range(boxes[i][0], boxes[i][1]):
+    for j in range(order[i][0], order[i][1]):
         temp = min(temp, remains[j])
-    temp = min(temp, boxes[i][2])
-    for k in range(boxes[i][0], boxes[i][1]):
+    temp = min(temp, order[i][2])
+    for k in range(order[i][0], order[i][1]):
         remains[k] -= temp
+    print(remains, temp)
     count += temp
 
 print(count)
