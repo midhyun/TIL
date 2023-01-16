e0aa58aa8e8c44479a23167fb5ac85c9092e8e32
midhyun <kfree912@gmail.com>
2023-01-16 19:48:58 +0900

    그리드_1201

---
+++ Python/문제풀기/after_bfs/1202_보석도둑.py
@@ -0,0 +1,25 @@
+import sys
+import heapq
+sys.stdin = open('1202_보석도둑.txt')
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+jewelry = []
+for _ in range(n):
+    m, v = map(int, input().split())
+    jewelry.append((m, v))
+heapq.heapify(jewelry)
+bags = []
+for _ in range(k):
+    bags.append(int(input()))
+bags.sort()
+result = 0
+temp = []
+for bag in bags:
+    while jewelry and bag >= jewelry[0][0]:
+        heapq.heappush(temp, -heapq.heappop(jewelry)[1])
+    if temp:
+        result -= heapq.heappop(temp)
+    elif not jewelry:
+        break
+print(result)
