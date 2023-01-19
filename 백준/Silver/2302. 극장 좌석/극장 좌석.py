1060275b844d114e35b6bdd03e41260d62cfe5bf
HyunJung Kim <108651809+midhyun@users.noreply.github.com>
2023-01-19 22:48:28 +0900

    [Silver I] Title: 극장 좌석, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Silver/2302. 극장 좌석/극장 좌석.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+M = int(input())
+vips = []
+temp = 1
+for _ in range(M):
+    vip = int(input())
+    vips.append(vip-temp)
+    temp = vip + 1
+vips.append(N-temp+1)
+max_fibo = max(vips)
+dp = [1]*(max_fibo+1)
+for i in range(2, max_fibo+1):
+    dp[i] = dp[i-1] + dp[i-2]
+result = 1
+for vip in vips:
+    result *= dp[vip]
+
+print(result)
