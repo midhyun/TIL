9d19a62e6c80732080e70c57f1b8d8f456c3bc13
midhyun <kfree912@gmail.com>
2023-01-20 12:17:39 +0900

    DP_스티커

--- Python/문제풀기/dp/temp.py
+++ Python/문제풀기/dp/temp.py
@@ -2,11 +2,5 @@ import sys
 sys.stdin = open('temp.txt')
 input = sys.stdin.readline
 
-n = int(input()) # n = 7
-dp = [0] * (n+1)
-for i in range(n):
-    a, b = map(int, input().split())
-    if i+a-1 < n:
-        dp[i+a-1] = max(dp[i+a-1], dp[i-1] + b)
-    dp[i] = max(dp[i-1], dp[i])
-print(dp[n-1])
+for i in range(1, 0):
+    print('yes')
