f6c909c3a1eb4fdc9ac305c32293c84998afe2f3
HyunJung Kim <108651809+midhyun@users.noreply.github.com>
2023-01-21 21:27:36 +0900

    [Silver I] Title: 오르막 수, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/11057. 오르막 수/오르막 수.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+lst = [[1]*10 for _ in range(n+1)]
+
+for i in range(2,n+1):
+    for j in range(10):
+        if j == 0:
+            lst[i][j] = sum(lst[i-1]) % 10007
+        else:
+            lst[i][j] = (lst[i][j-1] - lst[i-1][j-1]) %10007
+
+print(sum(lst[n])%10007)
