19d272e08f053014cd08a69fd0ddcc99d8c4e4a2
HyunJung Kim <108651809+midhyun@users.noreply.github.com>
2023-01-26 15:36:13 +0900

    [Silver III] Title: 돌 게임 3, Time: 112 ms, Memory: 114328 KB -BaekjoonHub

---
+++ 백준/Silver/9657. 돌 게임 3/돌 게임 3.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+dp = [0] * (n+5)
+dp[1], dp[2], dp[3], dp[4] = 1, 0, 1, 1
+
+for i in range(5, n+1):
+    if 0 in (dp[i-1], dp[i-3], dp[i-4]):
+        dp[i] = 1
+    else:
+        dp[i] = 0
+print('SK') if dp[n] else print('CY')
