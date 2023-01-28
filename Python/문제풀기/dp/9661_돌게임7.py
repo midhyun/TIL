9cab140e48e5aac668920bc479b1722087860f89
midhyun <kfree912@gmail.com>
2023-01-28 18:56:48 +0900

    돌게임7

--- Python/문제풀기/dp/9661_돌게임7.py
+++ Python/문제풀기/dp/9661_돌게임7.py
@@ -3,8 +3,7 @@ sys.stdin = open('9661_돌게임7.txt')
 input = sys.stdin.readline
 
 n = int(input())
-x = n%4
-if x == 2:
-    print('CY')
-else:
-    print('SK')
+
+x = n%5
+result = [0,1,0,1,1]
+print('SK') if result[x] else print('CY')
