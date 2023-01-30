d7267697e794b599f28a692fb24a4e5ec523407a
HyunJung Kim <108651809+midhyun@users.noreply.github.com>
2023-01-30 21:49:55 +0900

    [Silver V] Title: 방 번호, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/1475. 방 번호/방 번호.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+nums = [0]*10
+N = input().strip()
+
+for i in N:
+    nums[int(i)] += 1
+result = 0
+nums[6] += nums[9]
+for i in range(9):
+    num = nums[i]
+    if num == 0:
+        continue
+    if i == 6:
+        if num % 2 == 1:
+            num = (num+1)//2
+        else:
+            num //= 2
+    result = max(result, num)
+print(result)
