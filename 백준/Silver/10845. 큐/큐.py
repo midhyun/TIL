6fcac9ab4ff0f60909c5a00ba6473a2503c742d6
HyunJung Kim <108651809+midhyun@users.noreply.github.com>
2023-01-31 15:53:25 +0900

    [Silver IV] Title: 큐, Time: 80 ms, Memory: 34120 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,21 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+N = int(input())
+q = deque()
+
+for i in range(N):
+    order = input().split()
+    if order[0] == 'push':
+        q.append(order[1])
+    if order[0] == 'pop':
+        print(q.popleft()) if q else print(-1)
+    if order[0] == 'size':
+        print(len(q))
+    if order[0] == 'empty':
+        print(0) if q else print(1)
+    if order[0] == 'front':
+        print(q[0]) if q else print(-1)
+    if order[0] == 'back':
+        print(q[-1]) if q else print(-1)
