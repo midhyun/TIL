ac1de8e5f26a828036e0c768823ae7b589319a82
midhyun <kfree912@gmail.com>
2023-02-07 16:37:48 +0900

    theory_돌무더기

---
+++ Python/문제풀기/구현/24678_돌무더기게임1.py
@@ -0,0 +1,16 @@
+import sys
+sys.stdin = open('24678_돌무더기게임1.txt')
+input = sys.stdin.readline
+
+T = int(input())
+ans = ['B','R','R']
+for _ in range(T):
+    nums = [*map(int, input().split())]
+    cnt = 0
+    for num in nums:
+        if num % 2 == 1:
+            cnt += 1
+    if cnt < 2:
+        print('R')
+    else:
+        print('B')
