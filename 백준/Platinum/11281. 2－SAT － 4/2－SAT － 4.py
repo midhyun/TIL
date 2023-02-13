da35d4deb87f04140a01b97a1f645eecd4043e2c
HyunJung Kim <108651809+midhyun@users.noreply.github.com>
2023-02-13 20:11:24 +0900

    [Platinum III] Title: 2-SAT - 4, Time: 236 ms, Memory: 43324 KB -BaekjoonHub

---
+++ 백준/Platinum/11281. 2－SAT － 4/2－SAT － 4.py
@@ -0,0 +1,55 @@
+import sys
+sys.setrecursionlimit(10**7)
+input = sys.stdin.readline
+
+def dfs(cur):
+    global id, idx
+    id += 1
+    visited[cur] = id
+    parent = id
+    stack.append(cur)
+
+    for nxt in graph[cur]:
+        if visited[nxt] == -1:
+            parent = min(parent, dfs(nxt))
+        elif not on_stack[nxt]:
+            parent = min(parent, visited[nxt])
+    
+    if parent == visited[cur]:
+        idx += 1
+        while stack:
+            x = stack.pop()
+            on_stack[x] = 1
+            scc_idx[x] = idx
+            if x == cur:
+                break
+
+    return parent
+
+N, M = map(int, input().split())
+graph = [[] for _ in range(N*2+1)]
+for _ in range(M):
+    a, b = map(int, input().split())
+    graph[-a].append(b)
+    graph[-b].append(a)
+
+id, idx = 0, 0
+visited = [-1] * (N*2 + 1)
+on_stack = [0] * (N*2 + 1)
+scc_idx = [0] * (N*2 + 1)
+stack = []
+
+for i in range(1, N*2+1):
+    if visited[i] == -1:
+        dfs(i)
+
+result = [0]*N
+for i in range(1, N+1):
+    if scc_idx[i] == scc_idx[-i]:
+        print(0)
+        break
+    if scc_idx[i] < scc_idx[-i]:
+        result[i-1] = 1
+else:
+    print(1)
+    print(*result)
