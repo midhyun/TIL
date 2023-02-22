b488010db8dec6f5412f9f7a3ffeb30afe812f02
HyunJung Kim <108651809+midhyun@users.noreply.github.com>
2023-02-22 23:55:39 +0900

    [Gold III] Title: 고양이 목에 리본 달기, Time: 760 ms, Memory: 110376 KB -BaekjoonHub

---
+++ 백준/Gold/26093. 고양이 목에 리본 달기/고양이 목에 리본 달기.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+satis = []
+for i in range(N):
+    temp = [*map(int, input().split())]
+    satis.append(temp)
+
+dp = [[0]*K for _ in range(N)]
+dp[0] = satis[0]
+temp = sorted(dp[0])
+info = []
+
+for i in range(1, N):
+    tmp = (temp[-1], temp[-2])
+    for j in range(K):
+        if dp[i-1][j] == tmp[0]:
+            dp[i][j] = satis[i][j] + tmp[1]
+        else:
+            dp[i][j] = satis[i][j] + tmp[0]
+    temp = sorted(dp[i])
+
+print(max(dp[-1]))
