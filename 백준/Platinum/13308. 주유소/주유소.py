c69de3c28ae68a3539d6015305f3000d7f1456b7
HyunJung Kim <108651809+midhyun@users.noreply.github.com>
2023-03-16 22:26:00 +0900

    [Platinum V] Title: 주유소, Time: 268 ms, Memory: 164924 KB -BaekjoonHub

--- 백준/Platinum/13308. 주유소/주유소.py
+++ 백준/Platinum/13308. 주유소/주유소.py
@@ -2,21 +2,14 @@ import sys
 from heapq import heappop, heappush
 input = sys.stdin.readline
 
-INF = sys.maxsize
-N, M = map(int, input().split())
-costs = [0] + [*map(int, input().split())]
-graph = [[] for _ in range(N+1)]
-for _ in range(M):
-    a, b, c = map(int, input().split())
-    graph[a].append((b, c))
-    graph[b].append((a, c))
-dp = [[INF]*(max(costs)+1) for _ in range(N+1)]
-
 def dijkstra():
     q = []
+    dp = [[INF]*(max(costs)+1) for _ in range(N+1)]
     # 우선순위 큐에 (다음노드, 단위 비용, 총 비용) 값 삽입
+    dp[1][costs[1]] = 0
     heappush(q, (1, costs[1], 0))
     while q:
+        # dist : 현재 총 비용, cost : 단위 비용, cur : 현재 정점
         cur, cost, dist = heappop(q)
         # N번 노드에 도착했을경우 dist 리턴
         if cur == N: return dist
@@ -25,9 +18,18 @@ def dijkstra():
 
         # 현재위치에서 다음노드
         for nxt, nxt_dist in graph[cur]:
-            tmp = dist + cost * nxt_dist
-            if dp[nxt][cost] > tmp:
-                dp[nxt][cost] = tmp
-                heappush(q, (nxt, min(cost, costs[nxt]), tmp))
+            nxt_cost = min(costs[nxt], cost)
+            if dp[nxt][cost] > dist + cost * nxt_dist:
+                dp[nxt][cost] = dist + cost * nxt_dist
+                heappush(q, (nxt, min(cost, costs[nxt]), dist + cost * nxt_dist))
+
+INF = sys.maxsize
+N, M = map(int, input().split())
+costs = [-1] + [*map(int, input().split())]
+graph = [[] for _ in range(N+1)]
+for _ in range(M):
+    a, b, c = map(int, input().split())
+    graph[a].append((b, c))
+    graph[b].append((a, c))
 
 print(dijkstra())
