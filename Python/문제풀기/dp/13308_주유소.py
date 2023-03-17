019f8174170d4a44af08e2d5faddf57639c81d3d
midhyun <kfree912@gmail.com>
2023-03-17 18:30:58 +0900

    DP_주유소_다익스트라

--- Python/문제풀기/dp/13308_주유소.py
+++ Python/문제풀기/dp/13308_주유소.py
@@ -2,26 +2,32 @@ import sys
 from heapq import heappop, heappush
 sys.stdin = open('13308_주유소.txt')
 input = sys.stdin.readline
-
+# 음의간선이 없고 간선의 가중치가 있을 경우, 다익스트라
+# BFS를 수행하는데 큐대신 우선순위 큐를 활용함.
 def dijkstra():
     INF = sys.maxsize
     # dp[i][j] : i번도시까지, 단위비용j 일때 최소비용
     dp = [[INF]*(max(costs)+1) for _ in range(N+1)]
     q = []
     dp[1][costs[1]] = 0
+    # !! 힙자료구조 사용시 튜플의 첫번째 요소기준에 주의. !!
     heappush(q, (0, costs[1], 1))
     while q:
-        # now_dist : 총 비용, now_cost : 단위비용, now : 현재위치
-        now_dist, now_cost, now = heappop(q)
-        if now == N:
-            return now_dist
-        if dp[now][now_cost] < now_dist:
+        # dist : 총 비용, cost : 단위비용, cur : 현재위치
+        dist, cost, cur = heappop(q)
+        # 도착지까지의 총 비용 리턴
+        if cur == N:
+            return dist
+        # 이미 더 작은 총 비용일 경우 continue
+        if dp[cur][cost] < dist:
             continue
-        for next, next_dist in graph[now]:
-            next_cost = min(costs[next], now_cost)
-            if dp[next][now_cost] > now_dist + now_cost * next_dist:
-                dp[next][now_cost] = now_dist + now_cost * next_dist
-                heappush(q, (now_dist + now_cost * next_dist, next_cost, next))
+        # 현재 정점에서 bfs 수행
+        for next, next_dist in graph[cur]:
+            next_cost = min(costs[next], cost)
+            # 다음정점까지의 기존 비용보다, 현재정점을 거쳐서가는 비용이 더 적을때
+            if dp[next][cost] > dist + cost * next_dist:
+                dp[next][cost] = dist + cost * next_dist
+                heappush(q, (dist + cost * next_dist, next_cost, next))
 
 N, M = map(int, input().split())
 costs = [-1] + [*map(int, input().split())]
