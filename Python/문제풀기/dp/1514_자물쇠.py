ecb9480365f8066a7c8e6fb0f9cd47ecf64982bb
midhyun <kfree912@gmail.com>
2023-03-29 23:22:54 +0900

    DP_자물쇠_4차원

--- Python/문제풀기/dp/1514_자물쇠.py
+++ Python/문제풀기/dp/1514_자물쇠.py
@@ -1,24 +1,38 @@
 import sys
 sys.stdin = open('1514_자물쇠.txt')
 input = sys.stdin.readline
+sys.setrecursionlimit(1000000)
 
-N = int(input())  # 디스크의 개수
+n = int(input())
+s1, s2 = input().strip(), input().strip()
+a, b = [0] * 101, [0] * 101
+for i in range(1, n+1):
+    a[i] = int(s1[i-1])
+    b[i] = int(s2[i-1])
+# DP[i][i의 상태][i+1의 상태][i+2의 상태] = 최소 횟수
+dp = [[[[-1]*10 for _ in range(10)] for _ in range(10)] for _ in range(101)]
 
-cur = list(map(int, input().strip()))  # 현재 자물쇠의 상태
-pwd = list(map(int, input().strip()))  # 세준이의 비밀번호
+def DP(now, x, y, z):
+    if now > n:
+        return 0
+    ret = dp[now][x][y][z]
 
-min_rot = [0] * N  # 각 디스크에서의 최소 회전 횟수를 저장할 리스트
-total_rot = 0  # 최종 회전 횟수를 저장할 변수
-
-for i in range(N):
-    # 각 디스크에서의 최소 회전 횟수 계산
-    diff = abs(cur[i] - pwd[i])
-    min_rot[i] = min(diff, 10 - diff)
+    if ret != -1:
+        return ret
     
-    # 최소 회전 횟수를 계산하는 것은 세 개의 디스크를 동시에 회전하는 것이 가능
-    # 최대 세 개의 디스크를 묶어서 최소 회전 횟수를 계산
-    if i >= 2:
-        total_rot += min_rot[i-2:i+1]
+    ret = 1e9
+    ck = (b[now] - x + 10) % 10
+    d = [ck, 10 - ck]
+    
+    for i in range(2):
+        for j in range(d[i]+1):
+            for k in range(j+1):
+                yy = (y + (-j if i else j) + 10) % 10
+                zz = (z + (-k if i else k) + 10) % 10
+                val = DP(now+1, yy, zz, a[now+3])
+                val += (d[i] - j + 2) // 3 + (j - k + 2) // 3 + (k + 2) // 3
+                ret = min(ret, val)
+    dp[now][x][y][z] = ret
+    return ret
 
-# 최종 회전 횟수 출력
-print(min(total_rot))
+print(DP(1, a[1], a[2], a[3]))
