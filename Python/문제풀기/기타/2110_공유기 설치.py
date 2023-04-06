5d04287de504d6d770c696e7f387c62ab913ab05
midhyun <kfree912@gmail.com>
2023-04-06 20:46:40 +0900

    BinarySearch_공유기

---
+++ Python/문제풀기/기타/2110_공유기 설치.py
@@ -0,0 +1,31 @@
+import sys
+sys.stdin = open('2110_공유기 설치.txt')
+input = sys.stdin.readline
+
+N, C = map(int, input().split())
+houses = []
+for _ in range(N):
+    houses.append(int(input()))
+
+houses.sort()
+lo, hi = 1, houses[N-1] - houses[0]
+
+while lo < hi:
+    mid = (lo+hi)//2
+    cnt = 1
+    share = houses[0]
+
+    for i in range(N):
+        if houses[i] - share >= mid:
+            cnt += 1
+            share = houses[i]
+
+    if cnt >= C:
+        lo = mid + 1
+    else:
+        hi = mid
+
+if C == 2:
+    print(houses[N-1] - houses[0])
+else:
+    print(lo-1)
