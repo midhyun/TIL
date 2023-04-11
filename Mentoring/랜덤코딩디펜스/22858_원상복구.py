b0569adabd790c25e4cded2676bef0b9b9a7c7fe
midhyun <kfree912@gmail.com>
2023-04-11 23:10:41 +0900

    구현_역함수

---
+++ Mentoring/랜덤코딩디펜스/22858_원상복구.py
@@ -0,0 +1,22 @@
+import sys
+sys.stdin = open('22858_원상복구.txt')
+input = sys.stdin.readline
+
+def Reverse_string(x):
+    cur_S = x
+    for _ in range(K):
+        tmp = [0]*N
+        for i in range(N):
+            tmp[i] = cur_S[Rev_D[i]-1]
+        cur_S = tmp
+    return cur_S
+
+N, K = map(int, input().split())
+S = [*map(int, input().split())]
+D = [*map(int, input().split())]
+
+Rev_D = [0]*N
+for i in range(1, N+1):
+    Rev_D[D[i-1]-1] = i
+
+print(*Reverse_string(S))
