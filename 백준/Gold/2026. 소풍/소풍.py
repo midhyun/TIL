6de60aceab624f0696aa62855084e24b7724fe36
HyunJung Kim <108651809+midhyun@users.noreply.github.com>
2023-04-12 20:25:07 +0900

    [Gold III] Title: 소풍, Time: 64 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/2026. 소풍/소풍.py
@@ -0,0 +1,33 @@
+import sys
+sys.setrecursionlimit(10**7)
+input = sys.stdin.readline
+
+def dfs(friends, cur):
+    if len(friends) == K:
+        for friend in friends:
+            print(friend)
+        exit()
+
+    for nxt in range(cur+1, N+1):
+        if not visited[nxt]:
+            for friend in friends:
+                if friend not in graph[nxt]:
+                    break
+            else:
+                visited[nxt] = True
+                dfs(friends+[nxt], nxt)
+
+K, N, F = map(int, input().split())
+graph = [[] for _ in range(N+1)]
+for _ in range(F):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+
+for i in range(1, N+1):
+    visited = [False]*(N+1)
+    visited[i] = True
+    dfs([i], i)
+
+print(-1)
