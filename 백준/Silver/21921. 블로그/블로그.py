5a4796760e4e1e414484e86bbf2467e39d3f1897
HyunJung Kim <108651809+midhyun@users.noreply.github.com>
2023-04-14 18:20:07 +0900

    [Silver III] Title: 블로그, Time: 168 ms, Memory: 58920 KB -BaekjoonHub

---
+++ 백준/Silver/21921. 블로그/블로그.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+N, X = map(int, input().split())
+visited = [0] + [*map(int, input().split())]
+for i in range(1, N+1):
+    visited[i] += visited[i-1]
+def solution():
+    tmp = 0
+    tmp_lst = []
+    for i in range(N-X+1):
+        x = visited[i+X] - visited[i]
+        if tmp <= x:
+            tmp = x
+            tmp_lst.append(x)
+
+    if tmp:
+        print(tmp)
+        print(tmp_lst.count(tmp))
+    else:
+        print('SAD')
+
+solution()
