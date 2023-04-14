1bb124997588eaed1732aa1ca1b7c8293c02d7e9
HyunJung Kim <108651809+midhyun@users.noreply.github.com>
2023-04-14 22:46:25 +0900

    [Silver IV] Title: 카드 놓기, Time: 1396 ms, Memory: 120344 KB -BaekjoonHub

---
+++ 백준/Silver/5568. 카드 놓기/카드 놓기.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+
+def dfs(depth, cur):
+    if depth == K:
+        result.add(cur)
+    
+    for i in range(N):
+        if not check[i]:
+            check[i] = True
+            dfs(depth+1, cur+nums[i])
+            check[i] = False
+            
+N = int(input())
+K = int(input())
+nums = []
+for _ in range(N):
+    nums.append(input().rstrip())
+result = set()
+check = [False]*(N)
+
+
+for i in range(N):
+    check[i] = True
+    dfs(1, nums[i])
+    check[i] = False
+
+print(len(result))
