440c9a2c4624c9a19ee0fc76e19cf1c953b2acb0
HyunJung Kim <108651809+midhyun@users.noreply.github.com>
2023-04-16 00:13:08 +0900

    [Silver III] Title: 블로그2, Time: 96 ms, Memory: 32236 KB -BaekjoonHub

--- 백준/Silver/20365. 블로그2/블로그2.py
+++ 백준/Silver/20365. 블로그2/블로그2.py
@@ -4,18 +4,23 @@ input = sys.stdin.readline
 N = int(input())
 S = input().rstrip()
 
-cur = None
-cnt_B = 0
-cnt_R = 0
-for i in range(N):
-    if S[i] != cur:
-        cur = S[i]
-        if S[i] == 'B':
-            cnt_B += 1
-        else:
-            cnt_R += 1
 
-if S[0] == 'B':
-    print(cnt_R + 1)
-else:
-    print(cnt_B + 1)
+
+def solution():
+    cur = None
+    cnt_B = 0
+    cnt_R = 0
+    for i in range(N):
+        if S[i] != cur:
+            cur = S[i]
+            if S[i] == 'B':
+                cnt_B += 1
+            else:
+                cnt_R += 1
+
+    if S[0] == 'B':
+        return cnt_R + 1
+    else:
+        return cnt_B + 1
+
+print(solution())
