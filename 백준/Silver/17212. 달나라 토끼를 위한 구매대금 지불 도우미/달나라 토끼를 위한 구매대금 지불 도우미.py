84b44d6818ada2f4c19b996bf1a14449a77dd776
HyunJung Kim <108651809+midhyun@users.noreply.github.com>
2023-04-23 22:53:54 +0900

    [Silver III] Title: 달나라 토끼를 위한 구매대금 지불 도우미, Time: 96 ms, Memory: 35240 KB -BaekjoonHub

---
+++ 백준/Silver/17212. 달나라 토끼를 위한 구매대금 지불 도우미/달나라 토끼를 위한 구매대금 지불 도우미.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+coins = [1, 2, 5, 7]
+min_len = max(8, N+1)
+dp = [0]*(min_len)
+dp[1], dp[2], dp[3], dp[4], dp[5], dp[6], dp[7] = 1, 1, 2, 2, 1, 2, 1
+for i in range(8, N+1):
+    dp[i] = min(dp[i-1], dp[i-2], dp[i-5], dp[i-7]) + 1
+
+print(dp[N])
