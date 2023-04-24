8bbeeebf6c4f758652399176544efa6e5b92512f
midhyun <kfree912@gmail.com>
2023-04-25 00:43:37 +0900

    랜코디_다익스트라

---
+++ Mentoring/랜덤코딩디펜스/17396_백도어.py
@@ -0,0 +1,38 @@
+import sys
+from heapq import heappop, heappush
+sys.stdin = open('17396_백도어.txt')
+input = sys.stdin.readline
+
+INF = sys.maxsize
+N, M = map(int, input().split())
+sight = [*map(int, input().split())]
+sight[N-1] = 0
+graph = [[] for _ in range(N)]
+visited = [INF] * N
+for _ in range(M):
+    a, b, cost = map(int, input().split())
+    graph[a].append((cost, b))
+    graph[b].append((cost, a))
+
+def solution():
+    q = []
+    heappush(q, (0, 0))
+    while q:
+        dist, cur = heappop(q)
+        if dist > visited[cur]:
+            continue
+
+        if cur == N-1:
+            return visited[N-1]
+            
+        for cost, nxt in graph[cur]:
+            if sight[nxt]:
+                continue
+
+            tmp = cost + dist
+            if visited[nxt] > tmp:
+                visited[nxt] = tmp
+                heappush(q, (tmp, nxt))
+    return -1
+
+print(solution())
