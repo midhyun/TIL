65f5bd4dedb999873da59f227d684c764c0c14e2
midhyun <kfree912@gmail.com>
2023-04-27 01:26:10 +0900

    랜코디_재귀

--- Mentoring/랜덤코딩디펜스/17175_피보나치.py
+++ Mentoring/랜덤코딩디펜스/17175_피보나치.py
@@ -3,13 +3,14 @@ sys.stdin = open('17175_피보나치.txt')
 input = sys.stdin.readline
 result = 0
 def fibonacci(n):
-    global result
-    result += 1
-    result %= 1000000007
-    if (n < 2):
-        return n
+    if dp[n]:
+        return dp[n]
+    
+    dp[n] = (fibonacci(n-1) + fibonacci(n-2) + 1) % 1000000007
+    return dp[n]
 
-    return fibonacci(n-2) + fibonacci(n-1)
-
-fibonacci(int(input()))
-print(result)  
+N = int(input())
+dp = [0] * (N+4)
+dp[0], dp[1], dp[2] = 1, 1, 3
+fibonacci(N)
+print(dp[N])
