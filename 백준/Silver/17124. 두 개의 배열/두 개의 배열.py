451c961a1de691bfde94fd9fce3900e08b9168a9
HyunJung Kim <108651809+midhyun@users.noreply.github.com>
2023-04-27 20:12:38 +0900

    [Silver III] Title: 두 개의 배열, Time: 2496 ms, Memory: 46380 KB -BaekjoonHub

--- 백준/Silver/17124. 두 개의 배열/두 개의 배열.py
+++ 백준/Silver/17124. 두 개의 배열/두 개의 배열.py
@@ -1,43 +1,50 @@
 import sys
 input = sys.stdin.readline
 
-def findMin(d):
-  now = d[2]
-  rst = 2
-  if d[1] <= now:
-    now = d[1]
-    rst = 1
-  if d[0] <= now:
-    now = d[0]
-    rst = 0
-  return rst
- 
-def binary(cur, B, start, end):
-  diff = end - start
-  if diff <= 1:
-    return start
-  mid = (start + end) // 2
-  if cur < B[mid]:
-    return binary(cur, B, start, mid)
-  else: # cur >= B[mid]
-    return binary(cur, B, mid, end)
- 
-def solve():
-  n, m = [int(x) for x in input().split()]
-  A = [int(x) for x in input().split()]
-  B = [int(x) for x in input().split()]
-  B.sort()
-  s = 0
-  for cur in A:
-    p = binary(cur, B, 0, m)
-    d = []
-    d.append(abs(B[p-1] - cur))
-    d.append(abs(B[p] - cur))
-    d.append(abs(B[(p+1)%m] - cur))
-    idx = findMin(d)
-    s += B[p + idx - 1]
-  print(s)
+def find_min(arr):
+    cur = arr[2]
+    lt = 2
+    if arr[1] <= cur:
+        cur = arr[1]
+        lt = 1
+    elif arr[0] <= cur:
+        cur = arr[0]
+        lt = 0
 
-t = int(input())
-for _ in range(t):
-  solve()
+    return lt
+
+def binary_Search(s, e, B, x):
+    
+    while s < e:
+        diff = e - s
+        if diff <= 1:
+            return s
+        
+        mid = (s + e) // 2
+
+        if x < B[mid] :
+            e = mid
+        else:
+            s = mid
+
+    return s
+
+
+def solution():
+    N, M = map(int, input().split())
+    A = [*map(int, input().split())]
+    B = sorted([*map(int, input().split())])
+    c = 0
+    for i in range(N):
+        tmp = binary_Search(0, M-1, B, A[i])
+        arr = []
+        arr.append(abs(B[tmp-1] - A[i]))
+        arr.append(abs(B[tmp] - A[i]))
+        arr.append(abs(B[(tmp+1)%M] - A[i]))
+        idx = find_min(arr)
+        c += B[tmp + idx - 1]
+    print(c)
+
+T = int(input())
+for _ in range(T):
+    solution()
