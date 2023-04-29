4909c9912ea685c6a3a0ddf4f110add7fda2ab37
midhyun <kfree912@gmail.com>
2023-04-30 00:31:21 +0900

    랜코디_분할정복, 재귀

---
+++ Mentoring/랜덤코딩디펜스/1030_프렉탈평면.py
@@ -0,0 +1,24 @@
+import sys
+sys.stdin = open('1030_프렉탈평면.txt')
+input = sys.stdin.readline
+
+s, N, K, r1, r2, c1, c2 = map(int, input().split())
+I = N**s
+
+def check_b(I, x, y):
+    if I == 1:
+        return 0
+    center = I//N
+
+    if center * (N-K)//2 <= x < center * (N+K)//2 and center * (N-K)//2 <= y < center * (N+K)//2:
+        return 1
+    
+    x %= center
+    y %= center
+    
+    return check_b(I//N, x, y)
+
+for i in range(r1, r2+1):
+    for j in range(c1, c2+1):
+        print(check_b(I, i, j), end="")
+    print()
