ec88ec633ead9e54e7cc779dc8746a207d91d342
HyunJung Kim <108651809+midhyun@users.noreply.github.com>
2023-04-29 14:34:52 +0900

    [Silver III] Title: 두 스티커, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/16937. 두 스티커/두 스티커.py
@@ -0,0 +1,29 @@
+import sys
+input = sys.stdin.readline
+
+H, W = map(int, input().split())
+N = int(input())
+stickers = []
+for _ in range(N):
+    a, b = map(int, input().split())
+    stickers.append((a, b))
+
+result = 0
+
+for i in range(N):
+    r1, c1 = stickers[i]
+    r = H - r1
+    c = W - c1
+    sticker1 = r1*c1
+    for j in range(i+1, N):
+        r2, c2 = stickers[j]
+        if ((r1 + r2) <= H and max(c1, c2) <= W) or (max(r1, r2) <= H and c1 + c2 <= W ):
+            result = max(result, sticker1+(r2*c2))
+        if ((r1 + c2) <= H and max(c1, r2) <= W) or (max(r1, c2) <= H and c1 + r2 <= W ):
+            result = max(result, sticker1+(r2*c2))
+        if ((c1 + r2) <= H and max(r1, c2) <= W) or (max(c1, r2) <= H and r1 + c2 <= W ):
+            result = max(result, sticker1+(r2*c2))
+        if ((c1 + c2) <= H and max(r1, r2) <= W) or (max(c1, c2) <= H and r1 + r2 <= W ):
+            result = max(result, sticker1+(r2*c2))
+
+print(result)
