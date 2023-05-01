904a4cab1681679803af636b36b3589bbc6bc52e
HyunJung Kim <108651809+midhyun@users.noreply.github.com>
2023-05-01 20:37:21 +0900

    [Silver III] Title: 게임, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1072. 게임/게임.py
+++ 백준/Silver/1072. 게임/게임.py
@@ -5,25 +5,22 @@ X, Y = map(int, input().split())
 win_rate = (Y*100) // X
 
 def binary_Search():
-    Z = (Y*100) // X
-    if Z >= 99:
-        print(-1)
-    else:
-        answer = 0
-        left = 1
-        right = X
-    
-        while left <= right:
-            mid = (left + right) // 2
-            if (Y+mid)*100 // (X+mid) <= Z:
-                left = mid+1
-            else:
-                answer = mid
-                right = mid - 1
-    
-        print(answer)
+    lo = 1
+    hi = X
+    answer = 0
 
-if win_rate <= 99:
+    while lo <= hi:
+        mid = (lo + hi) // 2
+
+        if (Y+mid)*100 // (X+mid) <= win_rate:
+            lo = mid + 1
+        else:
+            answer = mid
+            hi = mid - 1 
+    
+    print(answer)
+
+if win_rate < 99:
     binary_Search()
 else:
     print(-1)
