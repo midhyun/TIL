32f0ed547654b85ebd0c9bb09da8f0c0916ea9fd
midhyun <kfree912@gmail.com>
2023-05-15 17:14:05 +0900

    랜코디_구현,브루트포스

---
+++ Mentoring/랜덤코딩디펜스/2428_표절.py
@@ -0,0 +1,28 @@
+import sys
+sys.stdin = open('2428_표절.txt')
+input = sys.stdin.readline
+
+N = int(input())
+files = sorted([*map(int, input().split())], reverse=True)
+
+def binary_Search(x):
+    lo = x + 1
+    hi = N
+    
+    while lo < hi:
+        mid = (lo + hi) // 2
+        
+        tmp = files[mid]/files[x]
+        
+        if tmp >= 0.9:
+            lo = mid + 1
+        else:
+            hi = mid
+    
+    return lo - 1
+
+result = 0
+for i in range(N):
+    result += binary_Search(i) - i
+
+print(result)
