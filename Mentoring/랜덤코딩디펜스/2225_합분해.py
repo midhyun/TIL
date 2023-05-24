278038a64fee7f94efbff00c113a945dfd80e29e
midhyun <kfree912@gmail.com>
2023-05-24 15:52:21 +0900

    랜코디_DP

--- Mentoring/랜덤코딩디펜스/2225_합분해.py
+++ Mentoring/랜덤코딩디펜스/2225_합분해.py
@@ -4,12 +4,12 @@ input = sys.stdin.readline
 
 N, K = map(int, input().split())
 dp = [[0]*(N+1) for _ in range(K+1)]
-for i in range(1, N+1):
+for i in range(N+1):
     dp[1][i] = 1
     
 for i in range(2, K+1):
-    for j in range(1, N+1):
-        for k in range(1, j+1):
+    for j in range(N+1):
+        for k in range(j+1):
             dp[i][j] += dp[i-1][j-k]
-            
-print(dp)
+        dp[i][j] %= 1000000000    
+print(dp[-1][-1])
