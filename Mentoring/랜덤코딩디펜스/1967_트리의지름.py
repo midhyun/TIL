da991b116d79b3a51175a7e53e2f1a3b4f42e440
midhyun <kfree912@gmail.com>
2023-05-25 23:18:30 +0900

    랜코디_DFS

---
+++ Mentoring/랜덤코딩디펜스/1967_트리의지름.py
@@ -0,0 +1,31 @@
+import sys
+sys.stdin = open('1967_트리의지름.txt')
+input = sys.stdin.readline
+N = int(input())
+graph = [[] for _ in range(N+1)]
+
+for _ in range(N-1):
+    a, b, c = map(int, input().split())
+    graph[a].append((b, c))
+    graph[b].append((a, c))
+
+def dfs(visited, cur, d):
+    res = 0
+    for nxt, dist in graph[cur]:
+        if visited[nxt] == -1:
+            visited[nxt] = d + dist
+            res = max(res, dfs(visited, nxt, d + dist) + dist)
+    
+    return res
+
+result = 0
+visited = [-1]*(N+1)
+visited[1] = 0
+dfs(visited, 1, 0)
+
+s_node = visited.index(max(visited))
+visited = [-1]*(N+1)
+visited[s_node] = 0
+dfs(visited, s_node, 0)
+
+print(max(visited))
