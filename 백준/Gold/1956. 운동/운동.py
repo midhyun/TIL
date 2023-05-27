4085b12774103c80f192c2027c7b0dfed641e178
HyunJung Kim <108651809+midhyun@users.noreply.github.com>
2023-05-28 01:04:53 +0900

    [Gold IV] Title: 운동, Time: 744 ms, Memory: 83340 KB -BaekjoonHub

--- 백준/Gold/1956. 운동/운동.py
+++ 백준/Gold/1956. 운동/운동.py
@@ -1,23 +1,32 @@
 import sys
+from heapq import heappush, heappop
 input = sys.stdin.readline
 
 V, E = map(int, input().split())
 INF = sys.maxsize
 matrix = [[] for _ in range(V+1)]
 graph = [[INF]*(V+1) for _ in range(V+1)]
-
+q = []
 for _ in range(E):
     a, b, c = map(int, input().split())
+    matrix[a].append((b, c))
     graph[a][b] = c
+    heappush(q, (c, a, b))
 
-for i in range(1, V+1):
-    for j in range(1, V+1):
-        for k in range(1, V+1):
-            graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
-result = INF
-for i in range(1, V+1):
-    result = min(result, graph[i][i])
-if result == INF:
-    print(-1)
+while q:
+    d, s, e = heappop(q)
+
+    if s == e:
+        print(d)
+        break
+
+    if graph[s][e] < d:
+        continue
+
+    for ng, dist in matrix[e]:
+        distance = d + dist
+        if distance < graph[s][ng]:
+            graph[s][ng] = distance
+            heappush(q, (distance, s, ng))
 else:
-    print(result)
+    print(-1)
