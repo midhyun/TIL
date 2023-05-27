9201b995ac92220cddc757f2503d148499c6751e
midhyun <kfree912@gmail.com>
2023-05-28 00:32:31 +0900

    랜코디_그래프 시뮬

--- Mentoring/랜덤코딩디펜스/21922_학부연구생민상.py
+++ Mentoring/랜덤코딩디펜스/21922_학부연구생민상.py
@@ -5,50 +5,38 @@ input = sys.stdin.readline
 
 N, M = map(int, input().split())
 matrix = [[*map(int, input().split())] for _ in range(N)]
-visited = [[[0]*4 for _ in range(M)] for _ in range(N)]
-dx, dy = [0, -1, 0, 1], [-1, 0, 1, 0]
-# 홀수: x축이동, 짝수: y축이동 [상, 좌, 하, 우]
+visited = [[0]*M for _ in range(N)]
+dy, dx = [1, -1, 0, 0], [0, 0, -1, 1]
+# 홀수: x축이동, 짝수: y축이동 [상, 하, 좌, 우]
 
 def find_start():
     for i in range(N):
         for j in range(M):
             if matrix[i][j] == 9:
-                visited[i][j] = [1, 1, 1, 1]
+                visited[i][j] = 1
                 for k in range(4):
-                    wind(k, (i, j))
+                    wind(k, i, j)
 
 
-def wind(dir, cur):
-    i, j = cur
-    y, x = i + dy[dir], j + dx[dir]
+def wind(dir, i, j):
+    ny, nx = dy[dir], dx[dir]
+    y, x = i + ny, j + nx
+    while 0 <= y < N and 0 <= x < M:
+        visited[y][x] = 1
 
-    if 0 <= y < N and 0 <= x < M and not visited[y][x][dir]:
-        if matrix[y][x] == 9:
-            return
-        visited[y][x][dir] = 1
-        if dir % 2 == 0:
-            if matrix[y][x] == 2:
-                return
-            elif matrix[y][x] == 3:
-                dir -= 1
-            elif matrix[y][x] == 4:
-                dir += 1
-        elif dir % 2 == 1:
-            if matrix[y][x] == 1:
-                return
-            elif matrix[y][x] == 3:
-                dir += 1
-            elif matrix[y][x] == 4:
-                dir -= 1
+        if matrix[y][x] == 3:
+            ny, nx = -nx, -ny
+        elif matrix[y][x] == 4:
+            ny, nx = nx, ny
+        elif (matrix[y][x] == 1 and ny == 0) or (matrix[y][x] == 2 and nx == 0):
+            break
+        
+        y += ny
+        x += nx
 
-        wind(dir%4, (y, x))
-
-
-start = find_start()
+find_start()
 
 result = 0
 for i in range(N):
-    for j in range(M):
-        if 1 in visited[i][j]:
-            result += 1
+    result += sum(visited[i])
 print(result)
