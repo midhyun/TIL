a946fad95c61a1f5856e99d51e1aa206e1756cb6
midhyun <kfree912@gmail.com>
2023-05-29 23:13:11 +0900

    랜코디_이분탐색

---
+++ Mentoring/랜덤코딩디펜스/17393_다이나믹롤러.py
@@ -0,0 +1,29 @@
+import sys
+from bisect import bisect
+sys.stdin = open('17393_다이나믹롤러.txt')
+input = sys.stdin.readline
+
+def binary_Search(s, e):
+    ink_ = ink_num[s]
+    result = e
+    while s < e:
+        mid = (s + e) // 2
+
+        if visco_num[mid] <= ink_:
+            s = mid + 1
+        else:
+            e = mid
+
+    return e
+
+
+N = int(input())
+ink_num = [*map(int, input().split())]
+visco_num = [*map(int, input().split())]
+
+for i in range(N):
+    print(binary_Search(i, N) - (i+1), end=' ')
+
+for i in range(N):
+    idx = bisect(visco_num, ink_num[i])
+    print(idx - (i+1), end= ' ')
