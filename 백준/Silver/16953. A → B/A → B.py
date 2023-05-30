0ce970fffdb52c0c81ea765318aa6971f8058088
HyunJung Kim <108651809+midhyun@users.noreply.github.com>
2023-05-31 01:01:55 +0900

    [Silver II] Title: A → B, Time: 104 ms, Memory: 35400 KB -BaekjoonHub

---
+++ 백준/Silver/16953. A → B/A → B.py
@@ -0,0 +1,21 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+A, B = map(int, input().split())
+
+def bfs():
+    q = deque()
+    q.append((A, 0))
+    while q:
+        x, cnt = q.popleft()
+        if x == B:
+            return cnt + 1
+
+        if x <= B:
+            q.append((x*2, cnt+1))
+            q.append((x*10+1, cnt+1))
+    
+    return -1
+
+print(bfs())
