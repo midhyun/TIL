7135f9e67e9122520c77b81d61d49cd85797132d
midhyun <kfree912@gmail.com>
2023-05-31 01:34:14 +0900

    랜코디_python_재귀 분할정복

---
+++ Mentoring/랜덤코딩디펜스/17829_222풀링.py
@@ -0,0 +1,22 @@
+import sys
+sys.stdin = open('17829_222풀링.txt')
+sys.setrecursionlimit(10**9)
+input = sys.stdin.readline
+
+N = int(input())
+graph = [[*map(int, input().split())] for _ in range(N)]
+
+def solution(N, i, j):
+    n = N//2
+    if N == 1:
+        return graph[i][j]
+    tmp = [
+        solution(n, i, j),
+        solution(n, i+n, j),
+        solution(n, i, j+n),
+        solution(n, i+n, j+n),
+    ]
+    tmp.sort()
+    return tmp[-2]
+
+print(solution(N, 0, 0))
