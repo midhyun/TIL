4850ecc515d33f19f8a303a7948416746f8a001b
midhyun <kfree912@gmail.com>
2023-06-07 01:11:56 +0900

    랜코디_dijkstra

--- Mentoring/랜덤코딩디펜스/14284_간선이어가기2.py
+++ Mentoring/랜덤코딩디펜스/14284_간선이어가기2.py
@@ -11,17 +11,36 @@ for _ in range(M):
     graph[a].append((b, c))
     graph[b].append((a, c))
 S, T = map(int, input().split())
-
+# 다익스트라 종료조건 부여
 def dijkstra(s, d):
+    visited = [INF]*(N+1)
+    q = []
+    heappush(q, (d, s))
+    while q:
+        d, cur = heappop(q)
+        if cur == T:
+            return d
+        for nxt, cost in graph[cur]:
+            dist = d + cost
+            if dist < visited[nxt]:
+                visited[nxt] = dist
+                heappush(q, (dist, nxt))
+
+print(dijkstra(S, 0))
+
+# 다익스트라 종료조건 미부여
+def dijkstra1(s, d):
     visited = [INF]*(N+1)
     q = []
     heappush(q, (d, s))
     visited[s] = 0
     while q:
         d, cur = heappop(q)
-        if cur == T:
-            return d
         for nxt, cost in graph[cur]:
-            heappush(q, (d + cost, nxt))
-            
-print(dijkstra(S, 0))
+            dist = visited[cur] + cost
+            if dist < visited[nxt]:
+                visited[nxt] = dist
+                heappush(q, (cost, nxt))
+    return visited[T]
+
+print(dijkstra1(S, 0))
