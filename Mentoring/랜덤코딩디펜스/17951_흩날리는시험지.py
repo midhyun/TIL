9ade00a63bb649adea59a32b4cca5fd9a827084c
midhyun <kfree912@gmail.com>
2023-06-08 23:54:34 +0900

    랜코디_이분탐색

--- Mentoring/랜덤코딩디펜스/17951_흩날리는시험지.py
+++ Mentoring/랜덤코딩디펜스/17951_흩날리는시험지.py
@@ -2,3 +2,27 @@ import sys
 sys.stdin = open('17951_흩날리는시험지.txt')
 input = sys.stdin.readline
 
+N, K = map(int, input().split())
+scores = [*map(int, input().split())]
+
+def binary_Search():
+    min_score = 0
+    max_score = 20*N
+    while min_score + 1 < max_score:
+        mid = (min_score+max_score)//2
+        group = 0
+        Totscore = 0
+        for i in range(N):
+            Totscore += scores[i]
+            if Totscore >= mid:
+                group += 1
+                Totscore = 0
+        
+        if group < K:
+            max_score = mid
+        else:
+            min_score = mid
+    
+    print(min_score)
+
+binary_Search()
