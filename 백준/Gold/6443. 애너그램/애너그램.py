e8fdd600160c535058fe9806cd0bce8fd473d6ea
HyunJung Kim <108651809+midhyun@users.noreply.github.com>
2023-06-16 00:04:44 +0900

    [Gold V] Title: 애너그램, Time: 376 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/6443. 애너그램/애너그램.py
@@ -0,0 +1,30 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+words = []
+for _ in range(N):
+    words.append(input().rstrip())
+
+
+def dfs(cur):
+    if cur == len(word):
+        print(''.join(answer))
+        return
+
+    for k in visited:
+        if visited[k]:
+            visited[k] -= 1
+            answer.append(k)
+            dfs(cur+1)
+            visited[k] += 1
+            answer.pop()
+    
+for word in words:
+    word = sorted(list(word))
+    visited = {}
+    answer = []
+    for i in word:
+        visited[i] = visited.get(i, 0) + 1
+
+    dfs(0)
