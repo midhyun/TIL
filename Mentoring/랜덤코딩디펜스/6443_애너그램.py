3168778fb574416fad7da8c7d5304ad3de92eb02
midhyun <kfree912@gmail.com>
2023-06-16 00:05:11 +0900

    랜코디_python_백트래킹_해쉬

--- Mentoring/랜덤코딩디펜스/6443_애너그램.py
+++ Mentoring/랜덤코딩디펜스/6443_애너그램.py
@@ -8,20 +8,24 @@ for _ in range(N):
     words.append(input().rstrip())
 
 
-def dfs(visited, cur):
-    if len(cur) >= len(visited):
-        tmp.add(cur)
+def dfs(cur):
+    if cur == len(word):
+        print(''.join(answer))
         return
 
-    for i in range(len(visited)):
-        if not visited[i]:
-            visited[i] = True
-            dfs(visited, cur+word[i])
-            visited[i] = False
+    for k in visited:
+        if visited[k]:
+            visited[k] -= 1
+            answer.append(k)
+            dfs(cur+1)
+            visited[k] += 1
+            answer.pop()
     
 for word in words:
-    visited = [False]*len(word)
-    tmp = set()
-    dfs(visited, '')
-    for x in sorted(list(tmp)):
-        print(x)
+    word = sorted(list(word))
+    visited = {}
+    answer = []
+    for i in word:
+        visited[i] = visited.get(i, 0) + 1
+
+    dfs(0)
