6b263bbe8773fd7dbb96e4f6fcdea9d5aca48505
midhyun <kfree912@gmail.com>
2023-06-26 20:00:21 +0900

    랜코디_python_Round

--- Mentoring/랜덤코딩디펜스/9613_GCD합.py
+++ Mentoring/랜덤코딩디펜스/9613_GCD합.py
@@ -1,3 +1,22 @@
 import sys
 sys.stdin = open('9613_GCD합.txt')
 input = sys.stdin.readline
+
+def find_GCD(a, b):
+    while True:
+        if b > a: a, b = b, a
+        remain = a % b
+        a, b = b, remain
+        if b == 0:
+            return a
+
+# 유클리드 호제법
+T = int(input())
+for _ in range(T):
+    integer = [*map(int, input().split())]
+    result = 0
+    for i in range(1, len(integer)):
+        for j in range(i+1, len(integer)):
+            result += find_GCD(integer[i], integer[j])
+    
+    print(result)
