3bad9ca5a70703c0555a05f5f0f9d54cfb4b0a9e
midhyun <kfree912@gmail.com>
2023-07-08 13:01:13 +0900

    connect_typeORM

--- Node/inflearn_node/NestJS_learn/src/app.module.ts
+++ Node/inflearn_node/NestJS_learn/src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BoardsModule } from './boards/boards.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeORMConfig } from './configs/typeorm.config';
 
 @Module({
-  imports: [BoardsModule],
+  imports: [
+    TypeOrmModule.forRoot(typeORMConfig),
+    BoardsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
