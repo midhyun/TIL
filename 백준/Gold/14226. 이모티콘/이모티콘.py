925c29931186d7c154c838266673ceb9d6f76213
HyunJung Kim <108651809+midhyun@users.noreply.github.com>
2023-11-27 19:02:48 +0900

    [Gold IV] Title: 이모티콘, Time: 128 ms, Memory: 38116 KB -BaekjoonHub

---
+++ 백준/Gold/14226. 이모티콘/이모티콘.py
@@ -0,0 +1,26 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+n = int(input())
+
+def bfs(n):
+    q = deque()
+    q.append((1,0,0))
+    set_ = set()
+    set_.add((1,0))
+    while q:
+        now, clip, times = q.popleft()
+        if now == n:
+            return times
+        if (now, now) not in set_:
+            q.append((now, now, times+1))
+            set_.add((now, now))
+        if clip != 0 and (now + clip, clip) not in set_:
+            q.append((now + clip, clip, times+1))
+            set_.add((now + clip, clip))
+        if (now-1, clip) not in set_:
+            q.append((now-1, clip, times+1))
+            set_.add((now-1, clip))
+
+print(bfs(n))
