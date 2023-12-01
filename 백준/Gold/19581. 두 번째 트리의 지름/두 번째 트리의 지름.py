a3d3beca7afbafd24094e41515715d27bd2842a7
HyunJung Kim <108651809+midhyun@users.noreply.github.com>
2023-12-01 22:45:10 +0900

    [Gold I] Title: 두 번째 트리의 지름, Time: 780 ms, Memory: 79368 KB -BaekjoonHub

---
+++ 백준/Gold/19581. 두 번째 트리의 지름/두 번째 트리의 지름.py
@@ -0,0 +1,34 @@
+import sys
+input = sys.stdin.readline
+
+def solution():
+    N = int(input())
+    node = [[] for _ in range(N+1)]
+    for _ in range(N-1):
+        i, j, d = map(int, input().split())
+        node[i].append((j, d))
+        node[j].append((i, d))
+    def dfs(far, rmv = 0):
+        distList = []
+        visited = [False]*(N+1)
+        stack = [(far, 0)]
+        visited[far] = True
+        visited[rmv] = True
+        while stack:
+            cur, curDist = stack.pop()
+            for nxt, dist in node[cur]:
+                if not visited[nxt]:
+                    visited[nxt] = True
+                    nxtDist = curDist + dist
+                    stack.append((nxt, nxtDist))
+                    distList.append((nxtDist, nxt))
+        distList.sort(reverse=True)
+        return distList
+    # 가장 끝 점
+    farDist, far = dfs(1)[0]
+    # 끝 점에서 지름
+    farDist, far1 = dfs(far)[0]
+    print(max(dfs(far, far1)[0][0], dfs(far1, far)[0][0]))
+
+
+solution()
