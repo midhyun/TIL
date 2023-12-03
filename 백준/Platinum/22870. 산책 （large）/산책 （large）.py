d2c33cfbf8528b78f7edde2b09a5b808a331741e
HyunJung Kim <108651809+midhyun@users.noreply.github.com>
2023-12-03 21:48:18 +0900

    [Platinum V] Title: 산책 (large), Time: 2700 ms, Memory: 181192 KB -BaekjoonHub

---
+++ 백준/Platinum/22870. 산책 （large）/산책 （large）.py
@@ -0,0 +1,52 @@
+import sys
+from heapq import heappop, heappush
+input = sys.stdin.readline
+
+def Solution():
+    INF = sys.maxsize
+    N, M = map(int, input().split())
+    node = [[] for _ in range(N+1)]
+    for _ in range(M):
+        i, j, dist = map(int, input().split())
+        node[i].append((j, dist))
+        node[j].append((i, dist))
+    for i in range(N+1): node[i].sort()
+    S, E = map(int, input().split())
+
+    def Dijkstra(visited, start, goal):
+        
+        distance = [INF]*(N+1)
+        distance[start] = 0
+        q = []
+        heappush(q, (0, start))
+        while q:
+            d, cur = heappop(q)
+            if distance[cur] < d: continue
+
+            if cur == goal:
+                return distance
+
+            for nxt, dist in node[cur]:
+                if nxt in visited: continue
+
+                ndist = d + dist
+                if ndist < distance[nxt]:
+                    distance[nxt] = ndist
+                    heappush(q, (ndist, nxt))
+
+    distance = Dijkstra(set(), E, S)
+    firstCost = 0
+    cursor = S
+    visited = set()
+    while cursor != E:
+        for nxt, cost in node[cursor]:
+            if firstCost + cost + distance[nxt] == distance[S]:
+                firstCost += cost
+                visited.add(nxt)
+                cursor = nxt
+                break
+    visited.remove(E)
+    distances = Dijkstra(visited, S, E)
+    print(firstCost + distances[E])
+
+Solution()
