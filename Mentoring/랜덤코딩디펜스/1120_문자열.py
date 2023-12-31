58ed56153b0dc0b7ff3b3e8ceddc2588dc27dd0a
midhyun <kfree912@gmail.com>
2023-12-31 16:52:31 +0900

    sol: (문자열) 1120_문자열

---
+++ Mentoring/랜덤코딩디펜스/1120_문자열.py
@@ -0,0 +1,14 @@
+import sys
+sys.stdin = open('1120_문자열.txt')
+input = sys.stdin.readline
+
+a, b = input().split()
+result = sys.maxsize
+for i in range(len(b)-len(a)+1):
+    cnt = 0
+    for j in range(len(a)):
+        if a[j] != b[j+i]:
+            cnt += 1
+    result = min(result, cnt)
+
+print(result)
