dec1fa333f373c0032ffcca9176dc8c450e08de7
HyunJung Kim <108651809+midhyun@users.noreply.github.com>
2024-01-02 20:49:40 +0900

    [Silver IV] Title: 파스칼 삼각형, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15489. 파스칼 삼각형/파스칼 삼각형.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+R, C, W = map(int, input().split())
+
+n = R + W - 1
+dp = [[0]*n for _ in range(n)]
+
+dp[0][0] = 1
+for i in range(1, n):
+    dp[i][0] = 1
+    dp[i][i] = 1
+
+for i in range(2, n):
+    for j in range(1, i):
+        dp[i][j] = dp[i-1][j-1] + dp[i-1][j]
+
+res = 0
+for i in range(W):
+    for j in range(i+1):
+        res += dp[i+R-1][j+C-1]
+print(res)
