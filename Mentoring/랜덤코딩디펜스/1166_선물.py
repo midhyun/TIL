0a6b0729a2c691a593a6d71a4d9aede97fcab439
midhyun <kfree912@gmail.com>
2024-01-09 00:24:06 +0900

    sol: (이분탐색) 1166_선물

---
+++ Mentoring/랜덤코딩디펜스/1166_선물.py
@@ -0,0 +1,18 @@
+import sys
+sys.stdin = open('1166_선물.txt')
+input = sys.stdin.readline
+
+N, L, W, H = map(int, input().split())
+
+def solution(n, l, w, h):
+    s, e = 0, min(l, w, h)
+    for _ in range(100):
+        mid = (s + e)/2
+        if (l//mid)*(w//mid)*(h//mid) >= n:
+            s = mid
+        else:
+            e = mid
+
+    print(f'{e:0.10f}')
+
+solution(N, L, W, H)
