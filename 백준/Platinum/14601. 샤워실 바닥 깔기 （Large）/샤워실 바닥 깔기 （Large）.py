f46f6064817bdbbc52d85454732e9c303c319ea5
HyunJung Kim <108651809+midhyun@users.noreply.github.com>
2024-01-09 22:29:18 +0900

    [Platinum V] Title: 샤워실 바닥 깔기 (Large), Time: 64 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Platinum/14601. 샤워실 바닥 깔기 （Large）/샤워실 바닥 깔기 （Large）.py
+++ 백준/Platinum/14601. 샤워실 바닥 깔기 （Large）/샤워실 바닥 깔기 （Large）.py
@@ -2,42 +2,41 @@ import sys
 input = sys.stdin.readline
 
 def check(y, x, size):
-    for i in range(y, y + size):
-        for j in range(x, x + size):
-            if matrix[i][j] != 0:
+    for i in range(y, y+size):
+        for j in range(x, x+size):
+            if board[i][j] != 0:
                 return False
     return True
 
-def solution(y, x, size):
+def solution(r, c, size):
     global id
-
     size = size >> 1
-    if check(y, x, size):
-        matrix[y+size-1][x+size-1] = id
-    if check(y + size, x, size):
-        matrix[y+size][x+size-1] = id
-    if check(y + size, x + size, size):
-        matrix[y+size][x+size] = id
-    if check(y, x + size, size):
-        matrix[y+size-1][x+size] = id
+    if check(r, c, size):
+        board[r+size-1][c+size-1] = id
+    if check(r, c+size, size):
+        board[r+size-1][c+size] = id
+    if check(r+size, c, size):
+        board[r+size][c+size-1] = id
+    if check(r+size, c+size, size):
+        board[r+size][c+size] = id
 
     id += 1
-
+    
     if size == 1:
         return
-    
-    solution(y, x, size)
-    solution(y+size, x, size)
-    solution(y, x+size, size)
-    solution(y+size, x+size, size)
+
+    solution(r, c, size)
+    solution(r+size, c, size)
+    solution(r, c+size, size)
+    solution(r+size, c+size, size)
 
 k = int(input())
 x, y = map(int, input().split())
-matrix = [[0]*(1<<k) for _ in range(1<<k)]
-matrix[(1 << k) - y][x-1] = -1
+board = [[0]*(1<<k) for _ in range(1<<k)]
+board[(1<<k)-y][x-1] = -1
 
 id = 1
+solution(0, 0, 1<<k)
 
-solution(0, 0, 1 << k)
-for m in matrix:
-    print(*m)
+for b in board:
+    print(*b)
