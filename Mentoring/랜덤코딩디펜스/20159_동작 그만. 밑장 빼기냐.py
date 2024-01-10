0ac900b6babc5dd40135b7f04f9b6368fd8b2a06
midhyun <kfree912@gmail.com>
2024-01-10 23:02:46 +0900

    sol : (누적합) 20159_동작그만 밑장빼기냐

---
+++ Mentoring/랜덤코딩디펜스/20159_동작 그만. 밑장 빼기냐.py
@@ -0,0 +1,31 @@
+import sys
+sys.stdin = open('20159_동작 그만. 밑장 빼기냐.txt')
+input = sys.stdin.readline
+
+def solution():
+    N = int(input())
+    cards = [*map(int, input().split())]
+    prefix_sum = [0]*N
+    
+    for i in range(-2, -N-1, -2):
+        prefix_sum[i] = prefix_sum[i+2] + cards[i]
+
+    ans = prefix_sum[0]
+    sum_cards = prefix_sum[0]
+
+    for i in range(N-1, 0, -2):
+        sum_cards += cards[i]
+        sum_cards -= cards[i-1]
+        ans = max(ans, sum_cards)
+    
+    sum_cards = prefix_sum[0]
+
+    for i in range(N-2, 1, -2):
+        sum_cards -= cards[i]
+        sum_cards += cards[i-1]
+        ans = max(ans, sum_cards)
+
+    print(ans)
+
+
+solution()
