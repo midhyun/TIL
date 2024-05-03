fea19e53b2c8af3089d0c18ad4de07269bc36000
HyunJung Kim <108651809+midhyun@users.noreply.github.com>
2024-05-03 18:01:36 +0900

    [Gold V] Title: 알약, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/4811. 알약/알약.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+def solution():
+    N = 30
+    dp = [[0]*(N+1) for _ in range(N+1)]
+    
+    for h in range(N+1):
+        for w in range(N+1):
+            if h > w: continue
+            if h == 0:
+                dp[w][h] = 1
+            else:
+                dp[w][h] = dp[w-1][h] + dp[w][h-1]
+    
+    while True:
+        N = int(input())
+        if N == 0:
+            return
+        print(dp[N][N])
+
+solution()
