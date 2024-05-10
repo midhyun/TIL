2c0fe74b2c91c7df7087a000140d401a48c92570
HyunJung Kim <108651809+midhyun@users.noreply.github.com>
2024-05-10 16:34:58 +0900

    [Gold V] Title: A와 B 2, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/12919. A와 B 2/A와 B 2.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+S = input().rstrip()
+T = input().rstrip()
+
+def solution(S, T):
+    if S == T:
+        print(1)
+        exit(0)
+    if len(S) >= len(T):
+        return
+
+    if T[-1] == 'A':
+        solution(S, T[:-1])
+    if T[0] == 'B':
+        solution(S, T[::-1][:-1])
+    
+
+
+solution(S, T)
+print(0)
