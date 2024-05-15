2cfa11cd8704703ad1daebeed4ace0b94e7e391e
midhyun <kfree912@gmail.com>
2024-05-15 18:02:44 +0900

    sol: (dp)4097_수익

---
+++ Mentoring/랜덤코딩디펜스/4097_수익.py
@@ -0,0 +1,23 @@
+import sys
+sys.stdin = open('4097_수익.txt')
+input = sys.stdin.readline
+
+def solution():
+    N = int(input())
+    if N == 0:
+        exit()
+
+    dp = [0]*N
+    earns = []
+    for i in range(N):
+        earn = int(input())
+        earns.append(earn)
+        if dp[i-1] + earn > 0:
+            dp[i] = dp[i-1] + earn
+    result = max(dp)
+    if result == 0:
+        result = max(earns)
+    print(result)
+
+while True:
+    solution()
