ac0fa01a6fb903fbe575be192ff2a18417efcc75
HyunJung Kim <108651809+midhyun@users.noreply.github.com>
2024-06-06 11:25:24 +0900

    [Gold V] Title: 1, 2, 3 더하기 4, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+
+def solution():
+    test_case = int(input())
+    dp = [0] * 10001
+    dp[1], dp[2], dp[3] = 1, 2, 3
+    numbers = []
+
+    for i in range(test_case):
+        numbers.append(int(input()))
+    
+    for i in range(4, max(numbers) + 1):
+        if i % 2 == 0:
+            cnt_2 = i // 2
+            temp = 1 + cnt_2 // 3
+        else:
+            cnt_2 = (i-3) // 2
+            temp = 1 + cnt_2 // 3
+
+        dp[i] = dp[i - 1] + temp
+    
+    for number in numbers:
+        print(dp[number])
+
+solution()
