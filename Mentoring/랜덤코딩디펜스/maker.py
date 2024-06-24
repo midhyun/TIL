3e46019bed143502f1c4bba63048e77aec88c10b
midhyun <kfree912@gmail.com>
2024-06-24 18:08:34 +0900

    메이커오류수정

--- Mentoring/랜덤코딩디펜스/maker.py
+++ Mentoring/랜덤코딩디펜스/maker.py
@@ -8,12 +8,15 @@ try:
     problem_number = input("Enter the problem number: ")
     driver = webdriver.Chrome()
     driver.get(f'https://www.acmicpc.net/problem/{problem_number}')
-    time.sleep(1)
-    problem_name = driver.find_element(By.XPATH, '//*[@id="problem_title"]').text
+    time.sleep(2)
+    problem_name = driver.find_element(By.ID, 'problem_title').text
+    print(problem_name)
     file_name = f'{problem_number}_{problem_name}'
-    safe_problem_name = re.sub('[^a-zA-Z0-9\n\.]', '', problem_name)
+    safe_problem_name = re.sub(r'[^a-zA-Z0-9\n\.\uAC00-\uD7A3]', '', problem_name)
+    print(safe_problem_name)
     file_name = f'{problem_number}_{safe_problem_name}'
     sample_input = driver.find_element(By.XPATH, '//*[@id="sample-input-1"]').text
+    driver.quit()
 
     python_file = file_name + ".py"
     with open(python_file, "x") as f:
@@ -34,3 +37,9 @@ except FileExistsError:
     print("File already exists.")
 except Exception as e:
     print(e)
+
+# try:
+#     os.system("taskkill /f /im chromedriver.exe /t")
+#     print(f"크롬드라이버 강제 종료 성공")
+# except Exception as e:
+#     print(f"크롬드라이버 강제 종료 실패")
