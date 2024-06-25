e29f2db897a1e5218dba8f95ba99e299a2a5eb4f
HyunJung Kim <108651809+midhyun@users.noreply.github.com>
2024-06-25 17:48:47 +0900

    [Gold III] Title: 두 배열의 합, Time: 408 ms, Memory: 121160 KB -BaekjoonHub

---
+++ 백준/Gold/2143. 두 배열의 합/두 배열의 합.py
@@ -0,0 +1,31 @@
+from collections import defaultdict
+import sys
+input = sys.stdin.readline
+
+def solution():
+    T = int(input())
+    n, A = int(input()), [*map(int, input().split())]
+    m, B = int(input()), [*map(int, input().split())]
+    answer = 0
+
+    sum_A, sum_B = [0], [0]
+    partialSumA, partialSumB = defaultdict(int), defaultdict(int)
+
+    for i in range(n):
+        sum_A.append(A[i] + sum_A[i])
+        for j in range(i+1):
+            partialSumA[sum_A[i+1] - sum_A[j]] += 1
+    
+    for i in range(m):
+        sum_B.append(B[i] + sum_B[i])
+        for j in range(i+1):
+            partialSumB[sum_B[i+1] - sum_B[j]] += 1
+    
+    for key in partialSumA.keys():
+        if T - key in partialSumB:
+            answer += partialSumA[key] * partialSumB[T - key]
+
+    print(answer)
+    return
+
+solution()
