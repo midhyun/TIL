34826755d848ff1a3e7c7ad24f80242a493d36ef
HyunJung Kim <108651809+midhyun@users.noreply.github.com>
2024-07-29 16:17:28 +0900

    [Gold IV] Title: 사촌, Time: 936 ms, Memory: 34060 KB -BaekjoonHub

--- 백준/Gold/9489. 사촌/사촌.py
+++ 백준/Gold/9489. 사촌/사촌.py
@@ -1,30 +1,29 @@
 import sys
+from collections import defaultdict
 input = sys.stdin.readline
 
-def solution():
+def solution2():
     while True:
         N, K = map(int, input().split())
-        if N == 0 and K == 0:
+        if (N, K) == (0, 0):
             break
+
         arr = [*map(int, input().split())]
-        children = [[] for _ in range(arr[-1]+1)]
-        parents = [i for i in range(arr[-1]+1)]
-        parent = -1
+        parent = defaultdict(int)
+
+        idx = -1
         for i in range(1, N):
             if arr[i] != arr[i-1]+1:
-                parent += 1
-            children[arr[parent]].append(arr[i])
-            parents[arr[i]] = arr[parent]
-        result = 0
-        if parents[K] == parents[parents[K]]:
-            print(0)
-            continue
-        for num in children[parents[parents[K]]]:
-            if num != parents[K]:
-                result += len(children[num])
+                idx += 1
+            parent[arr[i]]= arr[idx]
         
-        print(result)
+        cnt = 0
+        if parent[parent[K]]:
+            for node in arr:
+                if (parent[parent[K]] == parent[parent[node]]) and parent[node] != parent[K]:
+                    cnt += 1
+        
+        print(cnt)
 
-    return
+solution2()
 
-solution()
