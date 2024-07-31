b08535c5c69621648e65d36d8e5093303a4c4f31
midhyun <kfree912@gmail.com>
2024-07-31 15:00:36 +0900

    sol: (수학)부분수열의합_14225

---
+++ Mentoring/랜덤코딩디펜스/sumOfSubsequences_14225.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class sumOfSubsequences_14225 {
+    public static int N;
+    public static int[] numArr;
+    public static boolean[] visited, answer;
+
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("Mentoring/랜덤코딩디펜스/14225.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        numArr = new int[N];
+        answer = new boolean[2000001];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            numArr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(numArr);
+        int target = 0;
+        for (int num: numArr) {
+            if (target+1 < num) break;
+            target += num;
+        }
+        br.close();
+        System.out.println(target+1);
+
+    }
+}
