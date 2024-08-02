3d401f7972fe0db12b2c35ffa53d0157997b42c0
HyunJung Kim <108651809+midhyun@users.noreply.github.com>
2024-08-02 16:35:08 +0900

    [Gold IV] Title: 좋다, Time: 420 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/1253. 좋다/좋다.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+
+def solution():
+    N = int(input())
+    arr = [*map(int, input().split())]
+    arr.sort()
+    cnt = 0
+    for i in range(N):
+        s, e = 0, N-1
+        while s < e:
+            sum = arr[s] + arr[e]
+            if sum == arr[i]:
+                if s == i:
+                    s += 1
+                elif e == i:
+                    e -= 1
+                else:
+                    cnt += 1 
+                    break
+            elif sum > arr[i]:
+                e -= 1
+            else: s += 1
+    print(cnt)
+
+solution()
