1fb86971f09c9ee9a58198fdaf73fdd713f9ce7f
midhyun <kfree912@gmail.com>
2024-08-13 15:20:36 +0900

    sol: (bfs)13549_숨바꼭질3

---
+++ Mentoring/랜덤코딩디펜스/13549_숨바꼭질3.py
@@ -0,0 +1,19 @@
+import sys
+from heapq import heappop, heappush
+sys.stdin = open('13549_숨바꼭질3.txt')
+input = sys.stdin.readline
+
+def solution():
+    N, K = map(int, input().split())
+    visited = [False]*100001
+    q = [(0, N)]
+    while q:
+        t, x = heappop(q)
+        if x == K:
+            return t
+        for nxt in ((t, x*2), (t+1, x-1), (t+1, x+1)):
+            if 0<= nxt[1] <= 100000 and not visited[nxt[1]]:
+                visited[nxt[1]] = True
+                heappush(q, nxt)
+
+print(solution())
