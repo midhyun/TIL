ab88d27eea8c25d48c6dd3cf0138d18d2290e328
midhyun <kfree912@gmail.com>
2024-08-23 22:57:44 +0900

    sol: Refactor code for calculating the greatest common divisor

--- Mentoring/랜덤코딩디펜스/2824_최대공약수.py
+++ Mentoring/랜덤코딩디펜스/2824_최대공약수.py
@@ -11,12 +11,21 @@ n_nums = [*map(int, input().split())]
 M = int(input())
 m_nums = [*map(int, input().split())]
 
-answer = 1
-for i in range(N):
-    for j in range(M):
-        gcd_result = gcd(n_nums[i], m_nums[j])
-        answer *= gcd_result
-        n_nums[i] //= gcd_result
-        m_nums[j] //= gcd_result
+# answer = 1
+# for i in range(N):
+#     for j in range(M):
+#         gcd_result = gcd(n_nums[i], m_nums[j])
+#         answer *= gcd_result
+#         n_nums[i] //= gcd_result
+#         m_nums[j] //= gcd_result
 
-print(str(answer)[-9:])
+# print(str(answer)[-9:])
+
+n_num = 1
+m_num = 1
+for num in n_nums:
+    n_num *= num
+for num in m_nums:
+    m_num *= num
+
+print(str(gcd(n_num, m_num))[-9:])
