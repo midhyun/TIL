eec6767acc5a641175e882ee6515de3b173debfa
midhyun <kfree912@gmail.com>
2024-08-23 21:17:27 +0900

    sol: (수학)2824_최대공약수

---
+++ Mentoring/랜덤코딩디펜스/2824_최대공약수.py
@@ -0,0 +1,22 @@
+import sys
+sys.stdin = open('2824_최대공약수.txt')
+input = sys.stdin.readline
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+N = int(input())
+n_nums = [*map(int, input().split())]
+M = int(input())
+m_nums = [*map(int, input().split())]
+
+answer = 1
+for i in range(N):
+    for j in range(M):
+        gcd_result = gcd(n_nums[i], m_nums[j])
+        answer *= gcd_result
+        n_nums[i] //= gcd_result
+        m_nums[j] //= gcd_result
+
+print(str(answer)[-9:])
