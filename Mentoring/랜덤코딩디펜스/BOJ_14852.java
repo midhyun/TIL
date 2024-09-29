c090472f2d5afecbe574bad0c0575d00ad50b73d
midhyun <kfree912@gmail.com>
2024-09-29 23:04:10 +0900

    sol: (dp)14852_타일채우기3

---
+++ Mentoring/랜덤코딩디펜스/BOJ_14852.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_14852 {
+
+    private static final int MOD = 1000000007;
+    static long[][] dp;
+
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("Mentoring/랜덤코딩디펜스/14852.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        dp = new long[N+1][2];
+        System.out.println(findCase(N));
+
+        }
+
+    private static long findCase(int x) {
+
+        dp[0][0] = 0;
+        dp[1][0] = 2;
+        dp[2][0] = 7;
+        dp[2][1] = 1;
+
+        for (int i = 3; i <= x; i++) {
+            dp[i][1] = (dp[i-1][1] + dp[i-3][0]) % MOD;
+            dp[i][0] = (2 * dp[i-1][0] + 3* dp[i-2][0] + 2 * dp[i][1]) % MOD;
+        }
+
+        return dp[x][0];
+
+    }
+
+}
