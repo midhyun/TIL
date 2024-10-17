0f8494c7c76240351cb2b23aff2fd070d0cd2542
HyunJung Kim <108651809+midhyun@users.noreply.github.com>
2024-10-17 22:37:21 +0900

    [Gold V] Title: 두 개의 탑, Time: 176 ms, Memory: 19588 KB -BaekjoonHub

---
+++ 백준/Gold/2118. 두 개의 탑/두 개의 탑.java
@@ -0,0 +1,42 @@
+import java.io.*;
+
+public class Main {
+
+    static int N, left, right, leftDistance, rightDistance;
+    static int result;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+        arr[0] = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i < N; i++) {
+            arr[i] = arr[i-1] + Integer.parseInt(br.readLine());
+        }
+        result = left = right = 0;
+        while(right < N && left <= right) {
+            leftDistance = getLeft(left, right, arr);
+            rightDistance = getRight(leftDistance, arr);
+
+            if (leftDistance < rightDistance) {
+                right++;
+            } else {
+                left++;
+            }
+            result = Math.max(result, Math.min(leftDistance, rightDistance));
+        }
+
+
+        System.out.println(result);
+    }
+
+    public static int getLeft(int start, int end, int[] arr) {
+        return arr[end] - arr[start];
+    }
+
+    public static int getRight(int leftDistance, int[] arr) {
+        return arr[N-1] - leftDistance;
+    }
+}
