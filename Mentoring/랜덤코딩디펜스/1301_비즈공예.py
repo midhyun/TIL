9a03bc48b5eb648a48ce407158014f4762e31a65
midhyun <kfree912@gmail.com>
2024-10-24 22:19:18 +0900

    sol (dfs)17090_미로 탈출하기

---
+++ Mentoring/랜덤코딩디펜스/1301_비즈공예.py
@@ -0,0 +1,14 @@
+import sys
+sys.stdin = open('1301_비즈공예.txt')
+input = sys.stdin.readline
+
+def solution():
+
+    N = int(input())
+    beads = []
+    for _ in range(N):
+        beads.append(int(input()))
+    
+    return
+
+solution()
