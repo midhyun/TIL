5d72b3c6a0e2c11e1adc85e70892ea9565844694
midhyun <kfree912@gmail.com>
2024-11-18 22:25:28 +0900

    sol (union-find)7511_소셜 네트워킹 어플리케이션

---
+++ Mentoring/랜덤코딩디펜스/BOJ_7511.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_7511 {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("Mentoring/랜덤코딩디펜스/BOJ_7511.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int testCase = Integer.parseInt(br.readLine());
+        for (int i = 0; i < testCase; i++) {
+            int n = Integer.parseInt(br.readLine());
+            int k = Integer.parseInt(br.readLine());
+            int[] parent = new int[n+1];
+            for (int j = 1; j <= n; j++) {
+                parent[j] = j;
+            }
+            for (int j = 0; j < k; j++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+                unionParent(parent, a, b);
+            }
+            System.out.println("Scenario " + (i+1) + ":");
+            int m = Integer.parseInt(br.readLine());
+            for (int j = 0; j < m; j++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+                System.out.println(findParent(parent, a, b));
+            }
+            System.out.println();
+        }
+    }
+
+    static int getParent(int[] parent, int x) {
+        if(parent[x] == x) return x;
+        return parent[x] = getParent(parent, parent[x]);
+    }
+
+    static void unionParent(int[] parent, int a, int b) {
+        a = getParent(parent, a);
+        b = getParent(parent, b);
+        if(a < b) parent[b] = a;
+        else parent[a] = b;
+    }
+
+    static int findParent(int[] parent, int a, int b) {
+        a = getParent(parent, a);
+        b = getParent(parent, b);
+        if (a == b) return 1;
+        else return 0;
+    }
+}
